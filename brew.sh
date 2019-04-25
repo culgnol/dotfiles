e610674fa9759cd327b8d431da204d788162fe1b
Long Luc <culgnol@users.noreply.github.com>
2019-04-24 23:49:17 -0700

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -8,3 +8,12 @@ brew update
 # Upgrade any already-installed formulae
 brew upgrade
 
+# github util. gotta love `hub fork`, `hub create`, `hub checkout <PRurl>`
+# https://hub.github.com
+brew install hub
+
+# json parser - https://stedolan.github.io
+brew install jq
+
+# Remove outdated versions from the cellar
+brew cleanup
