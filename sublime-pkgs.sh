c8474b21aa70376b62e8e57a52b8ffbb7e6898cb
Long Luc <culgnol@users.noreply.github.com>
2019-04-25 22:44:20 -0700

    Update sublime-pkgs.sh

--- sublime-pkgs.sh
+++ sublime-pkgs.sh
@@ -3,18 +3,24 @@ cd ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages \
 
 mkdir -P ../Packages/User && cd ../Packages/User
 echo "{
-	\"installed_packages\":
-	[
-		\"Diffy\",
-    \"GitGutter\",
-    \"Indent XML\",
-		\"Package Control\",
-    \"Pretty JSON\"
-	]
+  \"installed_packages\":
+  [
+  \"AlignTab\",
+  \"BracketHighlighter\",
+  \"Diffy\",
+  \"GitGutter\",
+  \"Indent XML\",
+  \"Package Control\",
+  \"Pretty JSON\",
+  \"SideBarEnhancements\"
+  ]
 }
 " > "Package Control.sublime-settings"
 
+# AlignTab - https://github.com/randy3k/AlignTab
+# BracketHighlighter - https://github.com/facelessuser/BracketHighlighter
 # Diffy - https://github.com/zsong/diffy
 # GitGutter - https://jisaacks.github.io/GitGutter/
 # Indent XML - https://github.com/alek-sys/sublimetext_indentxml
 # Pretty JSON - https://github.com/dzhibas/SublimePrettyJson
+# SidebarEnhancements - https://github.com/titoBouzout/SideBarEnhancements
